e863137ab2fe4f38e39cbac54f9ec530c7f1e31c
andreeannn <andreeann2006@gmail.com>
2026-01-14 00:31:41 +0700

    Add files via upload

---
+++ web.php
@@ -0,0 +1,83 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('/unama', function () {
+    return view('unama');
+});
+
+Route::get('/1', function () {
+    return view('pertemuan1');
+});
+
+Route::get('/2', function () {
+    return view('pertemuan2');
+});
+
+Route::get('/3', function () {
+    return view('pertemuan3');
+});
+
+Route::get('/4', function () {
+    return view('pertemuan4');
+});
+
+Route::get('/5', function () {
+    return view('pertemuan5');
+});
+
+Route::get('/latihan', function () {
+    return view('latihan');
+});
+
+Route::get('/nama masing2.com', function () {
+    return view('nama');
+});
+
+Route::get('/unama.ac.id', function () {
+    return view('unama');
+});
+
+Route::get('/latihan3', function () {
+    return view('latihan3');
+});
+
+Route::get('/latihan4', function () {
+    return view('latihan4');
+});
+Route::get('/6', function () {
+    return view('Pertemuan6');
+});
+Route::get('/7', function () {
+    return view('Pertemuan7');
+});
+Route::get('/latihan6', function () {
+    return view('latihan6');
+});
+Route::get('/latihan7', function () {
+    return view('latihan7');
+});
+Route::get('/tugas1', function () {
+    return view('tugas1');
+});
+Route::get('/5.2', function () {
+    return view('pertemuan5(2)');
+});
+Route::get('/T1', function () {
+    return view('latihansayu');
+});
+Route::get('/uts', function () {
+    return view('ujian1');
+});
+Route::get('/Andreannn', function () {
+    return view('andrean');
+});
+Route::get('/for u', function () {
+    return view('frombayiik');
+});
+
